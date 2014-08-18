a85c9422b99c7370efb5580411415c7ce21c6178
Debian User <debian@debian-armhf.(none)>
2014-08-18 15:14:11 +0000

    MAJ chat - gestion scroll message - begin gestion list users

--- sockets/base.js
+++ sockets/base.js
@@ -3,17 +3,9 @@ module.exports = function (io) {
 
 	io.on('connection', function(socket){
 		socket.on('chat message', function(msg){
-			 
-			// io.get('pseudo', function (error, pseudo) {
-			// });
 			console.log(socket.pseudo + ' me parle ! Il me dit : ' + msg);
-			socket.broadcast.emit('chat message', socket.pseudo+" : "+msg);
-	
-		});
-
-		socket.on('disconnect', function(){
-			console.log('user disconnected');
-			socket.broadcast.emit('chat message', 'Utilisateur '+socket.pseudo+' deconnecte');
+			var ret = socket.pseudo+' : '+msg;
+			socket.broadcast.emit('chat message', ret);
 		});
 
 		socket.on('petit_nouveau', function(pseudo) {
@@ -21,5 +13,14 @@ module.exports = function (io) {
 			socket.broadcast.emit('chat message', 'Nouvel utilisateur connecte : '+socket.pseudo);
 			console.log('Nouvel utilisateur : '+socket.pseudo);
 		});
+
+	/*	
+		socket.on('disconnect', function(){
+			console.log('user disconnected');
+			socket.broadcast.emit('chat message', 'Utilisateur '+socket.pseudo+' deconnecte');
+		});
+	*/
 	});
+
+
 }
