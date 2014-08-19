f79174eef34441966c18f8b473b5d41c8e26b539
Anthony <anthony@localhost.localdomain>
2014-08-19 10:43:54 +0200

    add user handling

--- sockets/base.js
+++ sockets/base.js
@@ -1,23 +1,28 @@
 module.exports = function (io) { 
-	// io stuff here... io.on('conection..... 
+
+	var listUsers = [];
 
 	io.on('connection', function(socket){
-		socket.on('chat message', function(msg){
+		socket.on('chat_message', function(msg){
 			console.log(socket.pseudo + ' me parle ! Il me dit : ' + msg);
 			var ret = socket.pseudo+' : '+msg;
 			socket.broadcast.emit('chat message', ret);
 		});
 
-		socket.on('petit_nouveau', function(pseudo) {
+		socket.on('new_user', function(pseudo) {
 			socket.pseudo = pseudo;
-			socket.broadcast.emit('chat message', 'Nouvel utilisateur connecte : '+socket.pseudo);
+			listUsers.push(pseudo);
+			socket.broadcast.emit('chat_message', 'Nouvel utilisateur connecte : '+socket.pseudo);
+			socket.broadcast.emit('list_users', listUsers);
 			console.log('Nouvel utilisateur : '+socket.pseudo);
 		});
 
 		
 		socket.on('disconnect', function(){
 			console.log('user disconnected');
-			socket.broadcast.emit('chat message', 'Utilisateur '+socket.pseudo+' deconnecte');
+			var index = listUsers.indexOf(socket.pseudo);
+			listUsers.splice(index, 1);
+			socket.broadcast.emit('chat_message', 'Utilisateur '+socket.pseudo+' deconnecte');
 		});
 	
 	});
