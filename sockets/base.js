ba0482aa96b0694f9d4c404265ccba4140636507
Debian User <debian@debian-armhf.(none)>
2014-08-26 15:11:40 +0000

    handle code + socket io disconnection knowing -> to implement now

--- sockets/base.js
+++ sockets/base.js
@@ -3,28 +3,29 @@ module.exports = function (io) {
 	var listUsers = [];
 
 	io.sockets.on('connection', function(socket){
-		socket.on('chat_message', function(msg){
-			console.log('### - '+socket.user.pseudo + ' : ' + msg);
-			var ret = socket.user.pseudo+' : '+msg;
-			socket.broadcast.emit('chat_message', ret);
-		});
 		
-		socket.on('code_message', function(code){
-			socket.broadcast.emit('code_message', socket.user.pseudo, code);
-		});
-
 		socket.on('new_user', function(pseudo) {
-
 			socket.user = createObjectUser(pseudo);
-
-
 			listUsers.push(socket.user);
+
 			socket.broadcast.emit('chat_message', 'Nouvel utilisateur connecte : '+socket.user.pseudo);
 			socket.broadcast.emit('list_users', listUsers);
 			socket.emit('list_users', listUsers);
+
 			console.log('### - Nouvel utilisateur : '+socket.user.pseudo);
 		});
 
+		socket.on('chat_message', function(pseudo, msg){
+			console.log('### - '+socket.user.pseudo + ' : ' + msg);
+			socket.broadcast.emit('chat_message', socket.user.pseudo, msg);
+		});
+		
+		socket.on('code_message', function(pseudo, code){
+			console.log('### - User '+socket.user.pseudo+' sendong code');
+			socket.broadcast.emit('code_message', socket.user.pseudo, code);
+		});
+
+		
 		socket.on('error', function(obj){
 			console.log('error detected');
 			console.log("error : "+obj);
@@ -32,32 +33,27 @@ module.exports = function (io) {
 
 		
 		socket.on('disconnect', function(){
-			console.log("### - Socket user disconnected: "+socket.user.pseudo);
 			removeItem(listUsers, socket.user);
-			if(listUsers !== undefined || listUsers !== null){
-				socket.broadcast.emit('list_users', listUsers);
-			}
-			if(socket.user !== undefined || socket.user !== null){
-				socket.broadcast.emit('chat_message', 'Utilisateur '+socket.user.pseudo+' deconnecte');
-			}  
+			socket.broadcast.emit('list_users', listUsers);
+			socket.broadcast.emit('chat_message', 'Utilisateur '+socket.user.pseudo+' deconnecte');
+			console.log("### - Socket user disconnected: "+socket.user.pseudo);
 		});
 
+
+		// Add user
 		function createObjectUser(pseudo){
 			var user = {id: uuid.v4(),
 			pseudo: pseudo};
 			return user;
 		}
-
+		
+		// Remove user
 		function removeItem(array, item){
-    			if(array === undefined || array === null && item === undefined || item === null){
-				console.log("### - Nobody to disconnect !");
-			}else{
-				for (var i = array.length - 1; i >= 0; i--)
-				        if (array[i].id === item.id) {
-				            array.splice(i, 1);
-			        	    break; // remove this line if there could be multiple matching elements
-		        	}
-			}
+			for (var i = array.length - 1; i >= 0; i--)
+			        if (array[i].id === item.id) {
+			            array.splice(i, 1);
+		        	    break; // remove this line if there could be multiple matching elements
+		       	}
 		}
 	});
 }
